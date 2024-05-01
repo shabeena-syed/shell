b186d1d58b0843e2b487d526642c1ed31c70235f
shabeena.syed <syedfarzana589@gmail.com>
2024-05-02 01:27:04 +0530

    changed3

--- variables.sh
+++ variables.sh
@@ -4,8 +4,8 @@
 
 PERSON1:sonu
 PERSON2:10
-echo "$PERSON1 : hello,how r u
-echo "$PERSON2 : im good
-echo "$PERSON1 : how is ur practice
-echo "$PERSON2 : it is going good
-echo "$PERSON1 :its ok
+echo "$PERSON1 :: hello,how r u "
+echo "$PERSON2 :: im good"
+echo "$PERSON1 :: how is ur practice"
+echo "$PERSON2 :: it is going good"
+echo "$PERSON1 :: its ok"
