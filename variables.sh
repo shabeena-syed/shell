838a310723430043a7bc5c38996c4b059fb764da
shabeena.syed <syedfarzana589@gmail.com>
2024-05-01 22:03:02 +0530

    changes2

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# variables
+
+PERSON1:sonu
+PERSON2:10
+echo "$PERSON1 : hello,how r u
+echo "$PERSON2 : im good
+echo "$PERSON1 : how is ur practice
+echo "$PERSON2 : it is going good
+echo "$PERSON1 :its ok
