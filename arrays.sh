6b05612c10398cddeea47411186892cbdeb7738c
shabeena.syed <syedfarzana589@gmail.com>
2024-05-02 11:37:02 +0530

    arraylist

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#arrays 
+VALUES=("10","20","30")
+#size is 3
+#inex is 2
+echo "first value is : ${VALUES[0]}"
+echo "second value is :${VALUES[1]}"
+echo " third value is :${VALES[2]}"
