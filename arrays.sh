007fb1a608102975495866b4a89adef2b8a05153
shabeena.syed <syedfarzana589@gmail.com>
2024-05-02 11:58:54 +0530

    small changes

--- arrays.sh
+++ arrays.sh
@@ -3,6 +3,6 @@
 VALUES=("10","20","30")
 #size is 3
 #inex is 2
-echo "first value is : ${VALUES[0]}"
-echo "second value is :${VALUES[1]}"
-echo " third value is :${VALES[2]}"
+echo "first value is : ${VALUE[0]}"
+echo "second value is :${VALUE[1]}"
+echo " third value is :${VALUE[2]}"
